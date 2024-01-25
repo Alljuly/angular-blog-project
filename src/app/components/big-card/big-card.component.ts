061eb19b1bbc05a97c13d5b14d3e77d05ebf7025
alice <ajsc1@aluno.ifal.edu.brm>
2024-01-25 20:53:55 -0300

    adicionando atributos

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.css'
+  styleUrl: './big-card.component.css',
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
+  photoCover: string = '../../../assets/image-placeholder.jpg';
+  cardTitle: string = 'Adeus ao Lorem Ipsum tradicional';
+  cardDescription: string =
+    'Cat Ipsum deveria ser mais utilizado por todos! Pose purrfectly to show my beauty freak human out make funny noise mow mow mow mow mow mow success now attack human eat a plant, kill a hand i bet my nine lives on';
+  constructor() {}
 
+  ngOnInit(): void {}
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrl: './small-card.component.css'
+  styleUrl: './small-card.component.css',
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover: String = '';
+  @Input()
+  cardTitle: String = '';
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
