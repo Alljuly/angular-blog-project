f4456c4e0a150250e37c27f7c6242c6564a886ac
alice <ajsc1@aluno.ifal.edu.brm>
2024-01-25 19:18:05 -0300

    component: menu title

---
+++ src/app/components/menu-title/menu-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-title',
+  templateUrl: './menu-title.component.html',
+  styleUrl: './menu-title.component.css'
+})
+export class MenuTitleComponent {
+
+}
---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrl: './small-card.component.css'
+})
+export class SmallCardComponent {
+
+}
